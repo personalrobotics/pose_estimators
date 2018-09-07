e9f420e6f091c23d9606d92f1afd1c204ddbf73e
Daniel Gallenberger <danielga@cs.uw.edu>
2018-09-06 17:50:28 -0700

    update on weebo

--- src/deep_pose_estimators/external/load_checkpoints.sh
+++ src/deep_pose_estimators/external/load_checkpoints.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-scp -r prl@mthrbrn.personalrobotics.cs.washington.edu:/mnt/hard_data/GitHub/pytorch-retinanet-foods/checkpoint ./pytorch-retinanet/
-scp -r prl@mthrbrn.personalrobotics.cs.washington.edu:/mnt/hard_data/GitHub/pytorch-retinanet-foods/pretrained ./pytorch-retinanet/
+scp -r prl@mthrbrn.personalrobotics.cs.washington.edu:/mnt/hard_data/GitHub/pytorch-retinanet-foods/checkpoint ./pytorch_retinanet/
+scp -r prl@mthrbrn.personalrobotics.cs.washington.edu:/mnt/hard_data/GitHub/pytorch-retinanet-foods/pretrained ./pytorch_retinanet/
+
+scp -r prl@mthrbrn.personalrobotics.cs.washington.edu:/mnt/hard_data/GitHub/bite_selection_package/checkpoint ./bite_selection_package/
 
