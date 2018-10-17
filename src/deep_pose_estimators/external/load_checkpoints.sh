81c03e9c724983824c6f56df6f15eac0db1d7e37
Youngsun Kim <yskim041@gmail.com>
2018-10-17 13:00:30 -0700

    update links to checkpoint files on the cse server

--- src/deep_pose_estimators/external/load_checkpoints.sh
+++ src/deep_pose_estimators/external/load_checkpoints.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 echo "load checkpoints for spnet"
-wget -q https://personalrobotics.cs.washington.edu/shared_data/bite_selection_package/checkpoint/food_spnet_c6_dense_3_6_a_18_ckpt.pth -P ./bite_selection_package/checkpoint/
+wget https://personalrobotics.cs.washington.edu/files/food_manipulation/bite_selection_package/checkpoint/food_spnet_c6_dense_3_6_a_18_ckpt.pth -P ./bite_selection_package/checkpoint/
 
 echo "load checkpoints for retinanet"
-wget -q https://personalrobotics.cs.washington.edu/shared_data/pytorch_retinanet/checkpoint/food_c6_ckpt.pth -P ./pytorch_retinanet/checkpoint/
-wget -q https://personalrobotics.cs.washington.edu/shared_data/pytorch_retinanet/pretrained/food_c6_net.pth -P ./pytorch_retinanet/pretrained/
-
-
+wget https://personalrobotics.cs.washington.edu/files/food_manipulation/pytorch_retinanet/checkpoint/food_c6_ckpt.pth -P ./pytorch_retinanet/checkpoint/
+wget https://personalrobotics.cs.washington.edu/files/food_manipulation/pytorch_retinanet/pretrained/food_c6_net.pth -P ./pytorch_retinanet/pretrained/
