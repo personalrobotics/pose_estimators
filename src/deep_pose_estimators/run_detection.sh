a44e65aa581b00b803a5871820bf139ad940944e
Youngsun Kim <yskim041@gmail.com>
2018-10-18 15:40:40 -0700

    specify to use python2 for the detection script

--- src/deep_pose_estimators/run_detection.sh
+++ src/deep_pose_estimators/run_detection.sh
@@ -2,5 +2,5 @@
 
 source $(catkin locate)/devel/setup.bash
 
-python $(rospack find deep_pose_estimators)/src/deep_pose_estimators/detection_w_projection.py ada.json
+python2 $(rospack find deep_pose_estimators)/src/deep_pose_estimators/detection_w_projection.py ada.json
 
